c3329f38bdc675fc5bbed4cf85a2d28a8f5ef8ef
Jisoo Kim <jisoo000528@naver.com>
2024-10-03 23:35:00 +0900

    mugym added

---
+++ mugym/android/app/src/main/kotlin/com/example/mugym/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mugym
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mugym/lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Mugym!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
